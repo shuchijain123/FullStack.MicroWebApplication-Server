c53d31e077287d3f1cb31bda777d1da45beb560b
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-04-17 14:36:55 -0400

    tag tests

--- src/test/java/com/zipcode/wilmington/zipzapzopblog/model/TagModelTest.java
+++ src/test/java/com/zipcode/wilmington/zipzapzopblog/model/TagModelTest.java
@@ -45,5 +45,27 @@ public class TagModelTest {
         Assert.assertEquals(posts, tag.getPosts());
     }
 
+    @Test
+    public void testTagToString() {
+        //given
+        Tag tag = new Tag("TESTTAG123");
+        tag.setId(333L);
+        Post post = new Post();
+        post.setId(111L);
+        post.setTitle("TESTTITLE");
+        post.setBody("TESTBODY");
+        ArrayList<Post> posts = new ArrayList<>();
+        posts.add(post);
+        String expected = "Tag{id=333, keyWord='TESTTAG123', " +
+                            "posts=[Post{id=111, title='TESTTITLE', " +
+                            "body='TESTBODY', createDate=null, user=null, tags=[]}]}";
+        //when
+        tag.setPosts(posts);
+
+        //then
+        System.out.println(tag.toString());
+        Assert.assertEquals(expected, tag.toString());
+    }
+
 
 }
--- src/test/java/com/zipcode/wilmington/zipzapzopblog/service/TagServiceTest.java
+++ src/test/java/com/zipcode/wilmington/zipzapzopblog/service/TagServiceTest.java
@@ -1,11 +1,14 @@
 package com.zipcode.wilmington.zipzapzopblog.service;
 
+import com.zipcode.wilmington.zipzapzopblog.model.Post;
 import com.zipcode.wilmington.zipzapzopblog.model.Tag;
 import com.zipcode.wilmington.zipzapzopblog.repository.PostRepo;
 import com.zipcode.wilmington.zipzapzopblog.repository.TagRepo;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
@@ -57,7 +60,6 @@ public class TagServiceTest {
         TagService tagService = new TagService(tagRepo,postRepo);
         Tag tag = tagRepo.findById(3L).get();
 
-
         // call the method to test
         Tag actual = tagService.update(tag);
 
@@ -65,5 +67,85 @@ public class TagServiceTest {
         verify(tagRepo).save(tag);
     }
 
+    @Test
+    public void getTagTest(){
+        //given
+        Tag tagToReturn = new Tag("TEST TAG");
+        Long expected = 3L;
+        tagToReturn.setId(3L);
 
+        //mock
+        TagRepo tagRepo = mock(TagRepo.class);
+        PostRepo postRepo = mock(PostRepo.class);
+
+        // when actions
+        when(tagRepo.findById(3L)).thenReturn(Optional.of(tagToReturn));
+        // create class to test
+        TagService tagService = new TagService(tagRepo,postRepo);
+
+        // call the method to test
+        Tag actual = tagService.getTag(3L);
+
+        Assert.assertEquals(expected, actual.getId());
+    }
+
+    @Test
+    public void findAllTest(){
+        //given
+        Tag tagToReturn1 = new Tag("TEST TAG");
+        Long id1 = 3L;
+        tagToReturn1.setId(id1);
+        Tag tagToReturn2 = new Tag("TEST TAG2");
+        Long id2 = 3L;
+        tagToReturn2.setId(id2);
+        List<Tag> listOfTags = new ArrayList<>();
+        listOfTags.add(tagToReturn1);
+        listOfTags.add(tagToReturn2);
+
+        //mock
+        TagRepo tagRepo = mock(TagRepo.class);
+        PostRepo postRepo = mock(PostRepo.class);
+
+        // when actions
+        when(tagRepo.findAll()).thenReturn(listOfTags);
+
+        // create class to test
+        TagService tagService = new TagService(tagRepo,postRepo);
+
+
+        // call the method to test
+        List actual = tagService.findAll();
+
+        Assert.assertEquals(listOfTags, actual);
+    }
+
+    @Test
+    public void findPostTest(){
+        //given
+        Tag tagToReturn1 = new Tag("TEST TAG");
+        Long id1 = 3L;
+        tagToReturn1.setId(id1);
+        Post post = new Post();
+        post.setId(111L);
+        post.setTitle("TESTTITLE");
+        post.setBody("TESTBODY");
+        ArrayList<Post> posts = new ArrayList<>();
+        posts.add(post);
+
+        //mock
+        TagRepo tagRepo = mock(TagRepo.class);
+        PostRepo postRepo = mock(PostRepo.class);
+
+        // when actions
+        when(postRepo.findById(111L)).thenReturn(Optional.of(post));
+
+        // create class to test
+        TagService tagService = new TagService(tagRepo,postRepo);
+
+
+        // call the method to test
+        Optional<Post> actual = tagService.findPost(111L);
+
+        Assert.assertEquals(Optional.of(post), actual);
+    }
 }
