da9109e53e533c7a870983facecbd6457383e34b
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-04-20 18:47:03 -0400

    added ability to remove tags from posts

--- src/test/java/com/zipcode/wilmington/zipzapzopblog/controller/PostTagControllerTest.java
+++ src/test/java/com/zipcode/wilmington/zipzapzopblog/controller/PostTagControllerTest.java
@@ -102,7 +102,6 @@ public class PostTagControllerTest {
         Long expectedId = 3l;
         Tag tagToReturn = new Tag("TEST TAG");
         tagToReturn.setId(expectedId);
-        //List<Tag> listOfTags = Arrays.asList(tagToReturn);
         Long postId = 1L;
         post.setId(postId);
         post.setTitle("TESTTITLE");
@@ -160,6 +159,86 @@ public class PostTagControllerTest {
 
         // call the method to test
         ResponseEntity<Boolean> actual = postTagController.updatePostTag(postId,expectedId);
+
+        //verify the result
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, actual.getStatusCode());
+        verify(tagService).getTag(expectedId);
+        Assert.assertEquals(false, actual.getBody());
+    }
+
+
+
+    @Test
+    public void testDeletePostTag() {
+        //given
+        Tag tag = new Tag("TEST TAG");
+
+        //mock
+        TagService tagService = mock(TagService.class);
+        PostService postService = mock(PostService.class);
+        Post post = new Post();
+
+        //expected data
+        Long expectedId = 3l;
+        Tag tagToReturn = new Tag("TEST TAG");
+        tagToReturn.setId(expectedId);
+        Long postId = 1L;
+        post.setId(postId);
+        post.setTitle("TESTTITLE");
+        post.setBody("TESTBODY");
+
+        // when the method save is called with the tag
+        // then return the created tag
+        when(tagService.findPost(postId)).thenReturn(Optional.of(post));
+        when(postService.update(post)).thenReturn(post);
+        when(tagService.getTag(expectedId)).thenReturn(tagToReturn);
+
+        // create class to test
+        PostTagController postTagController = new PostTagController(tagService,postService);
+
+        // call the method to test
+        ResponseEntity<Boolean> actual = postTagController.deletePostTag(postId,expectedId);
+
+        //verify the result
+        Assert.assertEquals(HttpStatus.OK, actual.getStatusCode());
+        verify(tagService).getTag(expectedId);
+        Assert.assertEquals(true, actual.getBody());
+    }
+
+    @Test
+    public void testDeletePostTagBad() {
+        //given
+        Tag tag = new Tag("TEST TAG");
+
+        //mock
+        PostRepo postRepo = mock(PostRepo.class);
+        TagService tagService = mock(TagService.class);
+        PostService postService = new PostService(postRepo);
+        Post post = new Post();
+
+        //expected data
+        Long expectedId = 3l;
+        Tag tagToReturn = new Tag("TEST TAG");
+        tagToReturn.setId(expectedId);
+        List<Tag> listOfTags = Arrays.asList(tagToReturn);
+        Long postId = 111L;
+
+        post.setId(postId);
+        post.setTitle("TESTTITLE");
+        post.setBody("TESTBODY");
+
+        // when the method save is called with the tag
+        // then return the created tag
+        when(tagService.findPost(expectedId)).thenReturn(java.util.Optional.of(post));
+        when(postService.update(post)).thenReturn(post);
+        when(postRepo.save(post)).thenReturn(post);
+
+
+        // create class to test
+        PostTagController postTagController = new PostTagController(tagService,postService);
+
+        // call the method to test
+        ResponseEntity<Boolean> actual = postTagController.deletePostTag(postId,expectedId);
 
         //verify the result
         Assert.assertEquals(HttpStatus.BAD_REQUEST, actual.getStatusCode());
